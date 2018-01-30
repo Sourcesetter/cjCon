e6e9eb11dad473dc7f221a22ff6e822ade5a5763
Sourcesetter <christianjjohn@t-online.de>
2018-01-30 22:56:15 +0100

    trigger added

--- cjtests/pipe-skript/ex.sh
+++ cjtests/pipe-skript/ex.sh
@@ -1,1 +1,8 @@
-fly -t lite set-pipeline -p pipe-skript03 -c pipeline.yml 
+# Erstellen der Pipeline
+# fly -t lite set-pipeline -p pipe-skript04 -c pipeline.yml 
+
+# Triggern einers Jobs
+fly -t lite trigger-job -j pipe-skript04/job-exec-script
+
+# Ausgabe des Outputs auf das Terminal (task-name/job-name) 
+fly -t lite watch -j pipe-skript04/job-exec-script 
