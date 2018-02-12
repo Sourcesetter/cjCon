1a726939fa4d5b341e4ecb27a5b449e4c8430a86
Sourcesetter <christianjjohn@t-online.de>
2018-02-12 20:44:57 +0100

    bug fix

--- cjtests/task-io/create_some_files.sh
+++ cjtests/task-io/create_some_files.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+# Verzeichnis muss so heissen wie die Outputs im yml.
 mkdir some-files
 echo "Hallo file1-CJ" > some-files/file1
 echo "Hallo file2-CJ" > some-files/file2
 echo "Hallo file3-CJ" > some-files/file3
 echo "Hallo file4-CJ" > some-files/file4
 
-ls -alR some-files/*
+ls -als some-files/*
--- cjtests/task-io/sed_files.sh
+++ cjtests/task-io/sed_files.sh
@@ -2,10 +2,11 @@
 
 me=`basename $0`
 echo $me
-ls some-files/*
-cat some-files/file1 | sed 's/l/L/g' > some-files/file1.1
-cat some-files/file2 | sed 's/l/L/g' > some-files/file2.1
-cat some-files/file3 | sed 's/l/L/g' > some-files/file3.1
-cat some-files/file4 | sed 's/l/L/g' > some-files/file4.1
+
+mkdir some-sed-files
+cat some-files/file1 | sed 's/l/L/g' > some-sed-files/file1.1
+cat some-files/file2 | sed 's/l/L/g' > some-sed-files/file2.1
+cat some-files/file3 | sed 's/l/L/g' > some-sed-files/file3.1
+cat some-files/file4 | sed 's/l/L/g' > some-sed-files/file4.1
 echo "Verzeichnisstruktur"
-ls -alR /*
+ls -alR . 
