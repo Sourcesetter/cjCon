c3b2d61c2241f965cfb6a04384e8b36e314b0c29
Sourcesetter <christianjjohn@t-online.de>
2018-02-13 17:10:50 +0100

    Skript introduced

---
+++ cjtests/docker-images/ex_show_files.sh
@@ -0,0 +1,1 @@
+fly -t lite sp -p dockerImages -c pipeline_show_files.yml --var "docker-hub-email=christianjjohn@t-online.de" --var "docker-hub-username=christianjjohn" --var "docker-hub-password=W2b##G9A5B+6Q^7PKR@qr"
---
+++ cjtests/docker-images/show_files.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+me=`basename $0`
+echo $me
+
+set -e # fail fast
+set -x # print commands
+
+# Hier wird kein Docker-Image mit einem Git benötigt
+# Einfaches Download das gist-Verzeichnisses in den Docker-Container
+# git clone resource-gist some-sed-files-gist
+
+cd resource-gist
+ls *
+cat *
+
+echo "Verzeichnisstruktur nach cd .."
+cd ..
+ls -lR . 
